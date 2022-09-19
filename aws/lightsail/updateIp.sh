7ab7b651444286d2feda51ee9b0a90e0c0824d5a
Yo <37809541+yconway@users.noreply.github.com>
2022-09-19 01:10:28 -0500

    finish implementation

--- aws/lightsail/updateIp.sh
+++ aws/lightsail/updateIp.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 source ./config/dynamic-dns.cfg; # load the config
 
-echo USERNAME $USERNAME
-echo PASSWORD $PASSWORD
-echo DOMAIN $DOMAIN
-
-# IP=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
-# URL="https://${USERNAME}:${PASSWORD}@domains.google.com/nic/update?hostname=${DOMAIN}&myip=${IP}"
-# echo curling
-# echo $URL
-# curl -s $URL
+IP=$(curl http://checkip.amazonaws.com)
+URL="https://${USERNAME}:${PASSWORD}@domains.google.com/nic/update?hostname=${DOMAIN}&myip=${IP}"
+echo curling
+echo $URL
+curl -s $URL
